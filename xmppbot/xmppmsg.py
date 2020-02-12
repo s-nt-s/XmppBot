c43ce328783983f2269be64b8b799edcfb27b65b
s-nt-s <santos82h@gmail.com>
2020-02-12 17:34:48 +0100

    xmppmsg

--- xmppbot/xmppmsg.py
+++ xmppbot/xmppmsg.py
@@ -8,20 +8,6 @@ from .basebot import BaseBot
 from .common import get_config, to_xep0393
 
 
-class BaseBot(slixmpp.ClientXMPP):
-    def __init__(self, config_path):
-        self.config = get_config(config_path)
-        super().__init__(self.config.xmpp.user, self.config.xmpp.pssw)
-        logging.basicConfig(level=self.config.get(
-            'LOG', logging.INFO), format='%(levelname)-8s %(message)s')
-        self.log = logging.getLogger()
-
-    def run(self):
-        self.connect()
-        self.log.info("Bot started.")
-        self.process()
-
-
 class SendMsgBot(BaseBot):
     def __init__(self, config_path):
         super().__init__(config_path)
